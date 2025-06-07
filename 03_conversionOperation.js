ff80c986060d0038e6e315a948258cfcb7fcecdb
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-06-07 20:47:46 +0545

    Operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -23,3 +23,38 @@ let someValue = 77
 let stringValue = String(someValue); // Convert number to string
 console.log(typeof stringValue);
 console.log(stringValue); // "77"
+
+
+// Operations with different types
+let value = 3
+let negativeValue = -value; // Negation
+console.log(negativeValue); // -3
+
+let str1 = "Hello"
+let str2 = "World";
+let str3 = str1 + " " + str2; // String concatenation
+console.log(str3); // "Hello World"
+
+console.log("1" + 2); // "12" - string concatenation
+console.log(1 + "2"); // "12" - string concatenation
+console.log(1 + 2); // 3 - numeric addition
+console.log("1" - 2); // -1 - numeric subtraction
+console.log("1" + "2"); // "12" - string concatenation
+console.log("1" + 2 + 2); // "122" - string concatenation
+console.log("1" - 2 + 2); // 1 - numeric subtraction and addition
+console.log(1 + 2 + "2"); // "32" - numeric addition followed by string concatenation
+
+console.log((3 + 2) * 5 % 3); // 1 - arithmetic operations with precedence
+
+console.log(+true); // 1 - unary plus converts boolean to number
+console.log(+false); // 0 - unary plus converts boolean to number
+console.log(+""); // 0 - unary plus converts empty string to number
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++; // Increment gameCounter by 1. This is prefix increment
+// ++gameCounter; // Increment gameCounter by 1 again. // This is postfix increment
+console.log(gameCounter); // 101
